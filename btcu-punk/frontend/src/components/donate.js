2f04809ebbff59a7f5d0255e52aef2be8ad89364
Jakieyewang <306981258@qq.com>
2020-03-02 20:47:07 +0800

    day3 add some

---
+++ btcu-punk/frontend/src/components/donate.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Layout, Menu} from 'antd';
+import { exact } from 'prop-types';
+
+export default class donate extends React.Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
